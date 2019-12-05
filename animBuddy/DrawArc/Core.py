46213e10659343f81a61350ae3935f654a683c7b
darkuress <jhwang.ani@gmail.com>
2019-12-05 15:42:03 -0800

    modifying

--- animBuddy/DrawArc/Core.py
+++
@@ -1,6 +0,0 @@
-import maya.cmds as cmds
-
-def draw():
-    st = cmds.playbackOptions(q = True, min = True)
-    et = cmds.playbackOptions(q = True, max = True)
-    cmds.snapshot(motionTrail = True, increment  = 1, startTime = st, endTime = et)
