0a940f2fbd6b14f7b292af40b083d975856ab843
Jonghwan Hwang <jonghwanhwang@Jonghwans-MacBook-Pro.local>
2020-01-14 11:37:53 -0800

    fkik bake

--- animBuddy/FkIkMatch/Core.py
+++ animBuddy/FkIkMatch/Core.py
@@ -52,7 +52,7 @@ def getFromSelected(char, obj):
             long_name = one_obj
     return long_name
 
-def fkToIkConv(prefix = '', side = "LArm:"):
+def fkToIkConv(prefix = '', side = "LArm:", convert = True):
     """
     """
     char = getChar()
@@ -65,6 +65,7 @@ def fkToIkConv(prefix = '', side = "LArm:"):
     elbow_ctrl = getFromSelected(char, elbow_ctrl)
     wrist_ctrl = getFromSelected(char, wrist_ctrl)
     ik_ctrl = getFromSelected(char, ik_ctrl)
+    pv_ctrl = getFromSelected(char, prefix + side + Data.pv_ctrl)
 
     shoulder_pos = cmds.xform(shoulder_ctrl, t = True, q = True, ws = True)
     elbow_pos = cmds.xform(elbow_ctrl, t = True, q = True, ws = True) 
@@ -74,15 +75,17 @@ def fkToIkConv(prefix = '', side = "LArm:"):
     cmds.setAttr(blend_node + Data.blend_attr, 0)
     cmds.setAttr(blend_node + Data.pv_attr, 0)
 
-    cmds.xform(ik_ctrl, t = wrist_pos, ws = True)
+    #cmds.xform(ik_ctrl, t = wrist_pos, ws = True)
 
     temp_loc = cmds.spaceLocator()
     temp_pc = cmds.parentConstraint(wrist_ctrl, temp_loc, mo = False, weight = 1)
+    wrist_rot = cmds.xform(temp_loc, q = True, ro = True)
     cmds.delete(temp_pc)
-    temp_pc = cmds.parentConstraint(temp_loc, ik_ctrl, mo = False, weight = 1)
-    cmds.delete(temp_pc)
-    cmds.delete(temp_loc)
+    if convert:
+        temp_pc = cmds.parentConstraint(temp_loc, ik_ctrl, mo = False, weight = 1)
+        cmds.delete(temp_pc)
 
+    cmds.delete(temp_loc) 
     
     vec_sh_el = [elbow_pos[0] - shoulder_pos[0], elbow_pos[1] - shoulder_pos[1], elbow_pos[2] - shoulder_pos[2]]
     vec_wr_el = [elbow_pos[0] - wrist_pos[0], elbow_pos[1] - wrist_pos[1], elbow_pos[2] - wrist_pos[2]]
@@ -93,12 +96,22 @@ def fkToIkConv(prefix = '', side = "LArm:"):
     pv_destination = [elbow_pos[0] + 10*pv_destination[0], elbow_pos[1] + 10*pv_destination[1], elbow_pos[2] + 10*pv_destination[2]]
     temp_loc = cmds.spaceLocator()
     cmds.xform(temp_loc, t = pv_destination)
-
-    temp_pc = cmds.pointConstraint(temp_loc, prefix + side + Data.pv_ctrl, mo = False, weight = 1)
-    cmds.delete(temp_pc)
-    cmds.delete(temp_loc)
     
-def ikToFkConv(prefix = '', side = "LArm:"):
+    if convert:
+        temp_pc = cmds.pointConstraint(temp_loc, pv_ctrl, mo = False, weight = 1)
+        cmds.delete(temp_pc)
+    else:
+        cmds.setAttr(blend_node + Data.blend_attr, 1)
+
+    cmds.delete(temp_loc)
+  
+    return {'ik_pos'  : wrist_pos, 
+            'ik_rot'  : wrist_rot,
+            'ik_ctrl' : ik_ctrl,
+            'pv' : pv_destination,
+            'pv_ctrl' : pv_ctrl}
+    
+def ikToFkConv(prefix = '', side = "LArm:", convert = True):
     """
     """
     char = getChar()
@@ -118,20 +131,34 @@ def ikToFkConv(prefix = '', side = "LArm:"):
 
     shoulder_rot = cmds.xform(shoulder_jnt, q = True, ro = True)
     elbow_rot = cmds.xform(elbow_jnt, q = True, ro = True)
-
+    
     blend_node = getFromSelected(char, prefix + side + Data.blend_node)
     cmds.setAttr(blend_node + Data.blend_attr, 1)
-
-    cmds.xform(shoulder_ctrl, ro = shoulder_rot)
-    cmds.xform(elbow_ctrl, ro = elbow_rot)
 
     #wrist
     temp_loc = cmds.spaceLocator()
     temp_pc = cmds.parentConstraint(ik_ctrl, temp_loc, mo = False, weight = 1)
     cmds.delete(temp_pc)
-    temp_rc = cmds.orientConstraint(temp_loc, wrist_ctrl, mo = False, weight = 1)
-    cmds.delete(temp_rc)
+    wrist_rot = cmds.xform(temp_loc, q = True, ro = True)
+    wrist_pos = cmds.xform(temp_loc, q = True, t = True, ws = True)
+
+    if convert:
+        cmds.xform(shoulder_ctrl, ro = shoulder_rot)
+        cmds.xform(elbow_ctrl, ro = elbow_rot)
+        temp_rc = cmds.orientConstraint(temp_loc, wrist_ctrl, mo = False, weight = 1)
+        cmds.delete(temp_rc)
+    else:
+        cmds.setAttr(blend_node + Data.blend_attr, 0)
+
     cmds.delete(temp_loc)
+
+    return {'shoulder_rot'  : shoulder_rot, 
+            'elbow_rot'     : elbow_rot,
+            'wrist_rot'     : wrist_rot,
+            'wrist_pos'     : wrist_pos,
+            'shoulder_ctrl' : shoulder_ctrl,
+            'elbow_ctrl'    : elbow_ctrl,
+            'wrist_ctrl'    : wrist_ctrl}
 
 def convert(prefix = "", side = "LArm:"):
     """
@@ -145,4 +172,100 @@ def convert(prefix = "", side = "LArm:"):
         fkToIkConv(prefix = prefix, side = side)
     else:
         ikToFkConv(prefix = prefix, side = side)
+
+def bake(frame = []):
+    """
+    """
+    char = getChar()
+    prefix = getPrefix()
+    side = getSide()
+    blend_node = prefix + side + Data.blend_node
+    blend_node = getFromSelected(char, blend_node)
+    state = cmds.getAttr(blend_node + Data.blend_attr)
+
+    sel = cmds.ls(sl = True)[0]
+
+    if not frame:
+        frame = [int(cmds.playbackOptions(q = True, min = True)), int(cmds.playbackOptions(q = True , max = True))]
     
+    data = {}
+    cmds.refresh(su = True)
+    if state == 1:
+        #copy data
+        for fr in range(frame[0], frame[1]):
+            cmds.currentTime(fr)
+            data[fr] = fkToIkConv(prefix = prefix, side = side, convert = False)
+            cmds.select(sel)
+        cmds.setKeyframe(blend_node, attribute = Data.blend_attr, t = [frame[0] - 1, frame[0] -1])
+        
+        #paste data
+        temp_loc_ctrl = cmds.spaceLocator()
+        temp_loc_pv = cmds.spaceLocator()
+        for fr in range(frame[0], frame[1]):
+            #fk to ik
+            cmds.currentTime(fr)
+            cmds.setAttr(blend_node + Data.blend_attr, 0)
+            cmds.setKeyframe(blend_node, attribute = Data.blend_attr, t = [fr, fr])
+            cmds.xform(temp_loc_ctrl, t = data[fr]['ik_pos'], ro = data[fr]['ik_rot'])
+            cmds.xform(temp_loc_pv, t = data[fr]['pv'], ws = True)
+            cmds.setKeyframe(temp_loc_ctrl)
+            cmds.setKeyframe(temp_loc_pv)
+        temp_con_ctrl = cmds.parentConstraint(temp_loc_ctrl, data[fr]['ik_ctrl'], mo = False, weight = 1)
+        temp_con_pv = cmds.pointConstraint(temp_loc_pv, data[fr]['pv_ctrl'], mo = False, weight = 1)
+        for item in [data[fr]['ik_ctrl'], data[fr]['pv_ctrl']]:
+            cmds.bakeResults(item, 
+                             simulation = True,
+                             t = (frame[0], frame[1]),
+                             sampleBy = 1, 
+                             oversamplingRate = 1, 
+                             disableImplicitControl= True,
+                             preserveOutsideKeys = True,
+                             sparseAnimCurveBake = False,
+                             removeBakedAttributeFromLayer = False,
+                             removeBakedAnimFromLayer = False,
+                             minimizeRotation = True,
+                             controlPoints = False,
+                             shape = True)
+
+        cmds.delete(temp_con_ctrl + temp_con_pv)
+        cmds.delete(temp_loc_ctrl + temp_loc_pv)
+        cmds.refresh(su = False)
+        cmds.setAttr(blend_node + Data.blend_attr, 1)
+        cmds.setKeyframe(blend_node, attribute = Data.blend_attr, t = [frame[1] + 1, frame[1] + 1])
+    
+    elif state == 0:
+        #copy data
+        for fr in range(frame[0], frame[1]):
+            cmds.currentTime(fr)
+            data[fr] = ikToFkConv(prefix = prefix, side = side, convert = False)
+            cmds.select(sel)
+        cmds.setKeyframe(blend_node, attribute = Data.blend_attr, t = [frame[0] - 1, frame[0] -1])
+        #paste data
+        temp_loc = cmds.spaceLocator()
+        for fr in range(frame[0], frame[1]):
+            #fk to ik
+            cmds.currentTime(fr)
+            cmds.setAttr(blend_node + Data.blend_attr, 1)
+            cmds.setKeyframe(blend_node, attribute = Data.blend_attr, t = [fr, fr])
+            cmds.xform(data[fr]['shoulder_ctrl'], ro = data[fr]['shoulder_rot'])
+            cmds.xform(data[fr]['elbow_ctrl'], ro = data[fr]['elbow_rot'])
+            cmds.xform(temp_loc, t = data[fr]['wrist_pos'], ro = data[fr]['wrist_rot'])
+            cmds.setKeyframe(temp_loc)
+        temp_con = cmds.orientConstraint(temp_loc, data[fr]['wrist_ctrl'], mo = False, weight = 1)
+        cmds.bakeResults(data[fr]['wrist_ctrl'], 
+                         simulation = True,
+                         t = (frame[0], frame[1]),
+                         sampleBy = 1, 
+                         oversamplingRate = 1, 
+                         disableImplicitControl= True,
+                         preserveOutsideKeys = True,
+                         sparseAnimCurveBake = False,
+                         removeBakedAttributeFromLayer = False,
+                         removeBakedAnimFromLayer = False,
+                         minimizeRotation = True,
+                         controlPoints = False,
+                         shape = True) 
+        cmds.delete(temp_con)
+        cmds.refresh(su = False)
+        cmds.setAttr(blend_node + Data.blend_attr, 0)
+        cmds.setKeyframe(blend_node, attribute = Data.blend_attr, t = [frame[1] + 1, frame[1] + 1])           
